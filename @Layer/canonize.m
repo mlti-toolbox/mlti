8e7ee4aa0027cf14bb3b395d0e9c725eb4678335
Joshua Kelley <60236515+k-joshua-kelley@users.noreply.github.com>
2025-08-28 14:12:07 -0600

    Delete @Layer/canonize.m

--- @Layer/canonize.m
+++
@@ -1,5 +0,0 @@
-function [K11, K21, K31, K22, K32, K33, C, a, R, h] = canonize(obj, varargin)
-    assert(nargin == obj.Nin, "Invalid number of input arguments. Expected %d, got %d.", obj.Nin, nargin)
-    [K11, K21, K31, K22, K32, K33] = obj.ko2K(varargin);
-    [C, a, R, h] = obj.getLast4(varargin);
-end
